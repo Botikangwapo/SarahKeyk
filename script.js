5ec2d5029440e6236ae58174dc0704b4876577be
mjdev <jervinvillones6@gmail.com>
2026-03-20 21:10:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const btntabs = document.querySelectorAll(".btnmv");
+const contenttabs = document.querySelectorAll(".content-mv");
+const serachbar = document.getElementById("searchbar");
+const result = document.getElementById("result");
+
+
+// Dommanipulation scroll effect
+document.addEventListener("scroll", () =>{
+    const head = document.querySelector('.head');
+
+    if (window.scrollY > 0){
+        head.classList.add("close")
+    }else{
+        head.classList.remove("close")
+         result.classList.remove("hidden");
+    }   
+});
+
+// for drop down in searchbar
+serachbar.addEventListener("click", () => {
+  result.classList.toggle("hidden");
+});
+
+// drop down links of searchbar
+document.querySelectorAll("#result li").forEach(item => {
+  item.addEventListener("click", () => {
+    const link = item.getAttribute("data-link");
+    window.location.href = link;
+  });
+});
+
+// tab nevigation for mision vision explain ko paba
+btntabs.forEach((tab, index) => { 
+    tab.addEventListener('click', (e) => {
+        btntabs.forEach(tab=>{
+            tab.classList.remove('active');
+        });
+        tab.classList.add('active');
+
+         var line = document.querySelector('.line')
+         line.style.width = e.target.offsetWidth + "px";
+         line.style.left = e.target.offsetLeft + "px";
+
+         contenttabs.forEach(contents=>(contents.classList.remove('active')))
+         contenttabs[index].classList.add('active')
+    });
+});
+
+
+
